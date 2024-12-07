1849e3bd59c9857ec47765bc29e8ffad42375343
omran <omran.abandeh@pwc.com>
2024-12-07 12:12:54 +0300

    Login Mobile mode feature

---
+++ frontend/src/json/static/staticSignupPage.ts
@@ -0,0 +1,11 @@
+const firstNameLabel = "Enter your first name";
+const firstNamePlaceholder = "Enter your first name";
+const secondNameLabel = "Enter your second name";
+const secondNamePlaceholder = "Enter your second name";
+
+export {
+  firstNameLabel,
+  firstNamePlaceholder,
+  secondNameLabel,
+  secondNamePlaceholder,
+};
