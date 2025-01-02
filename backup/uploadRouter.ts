cd726ea21ba0dba1ad5e85737e82f4d9b73ccc1e
omran <omran.abandeh@pwc.com>
2025-01-02 18:26:31 +0300

    remove bk folder

--- backup/uploadRouter.ts
+++
@@ -1,8 +0,0 @@
-
-
-
-// Create task a POST route
-router.post("/add/image", upload.single("file"),verifyToken, uploadImageController);
-
-// Create task a POST route
-router.post("/test", verifyToken, upload.single("file"), test);
